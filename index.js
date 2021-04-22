226fc48e609d5a123de700cac18509ba8cf63f96
Jared <jaralleg@amazon.com>
2021-04-22 10:51:22 -0400

    Creating and appending elements

--- index.js
+++ index.js
@@ -1,50 +1,70 @@
- // Get value from zip code form
- // look up location id (woeid) in API
- //Use location ID to search for forecast info
- //Display forcast info on the page
- //Format and style
- //Begin stretch goals
-
-
- /*
+/* // test fetch statment
  fetch('https://meta-weather.vercel.app/api/location/search/?query=san', {
 })
   .then(res => res.json())
   .then(console.log)
 */
 
- //add event listner to form submit 
- let location = document.getElementById('location').value
+document.addEventListener('DOMContentLoaded', (e) => {
+    console.log('DOM fully loaded and parsed');
 
-const configObj = {
-    method: 'GET',
-    headers: { 'Content-Type': 'application/json' },
-    mode: 'no-cors'
-  };
+    function containers(){
+        //grab the divs 
+        const searchContainer = document.getElementById('search-container');
+        const weatherContainer = document.getElementById('weather-container');
+    }
 
-  //Pull 5 day forecast from API based on zip
- let API = 'https://meta-weather.vercel.app/api/location/search/?query='+location
-  function getlocation(){ 
-   return fetch( API, configObj)
-     .then(response => response.json())
-     .then(data => console.log(data));
-          //get woeid and send to getWeather()
- }  
+    //Create the search box and button
+    function createSearchBox(){
+        const searchBox = document.createElement("INPUT").autofocus;
+        searchBox.setAttribute("type", "text");
+        
+        const submitBtn = document.createElement("BUTTON");
+        submitBtn.innerText = 'Submit'
+        let location = document.getElementById('searchBox').value;
+        //submitBtn.addEventListner('click', getlocation)  
+    }
 
+    function createForecast(){
+        console.log('forecast placeholder');
+    }
 
-  function getWeather(){ 
-   return fetch('https://www.metaweather.com//api/location/'+woeid,
-   {
-    method: 'GET',
-    headers: { 'Content-Type': 'application/json' },
-    mode: 'no-cors'
-  })
-     .then(response => response.json())
-     .then(data => console.log(data));
+    function appendForecast(){
+        console.log('forecast placeholder');
 
- }  
+    }
+    function appendToSeachContainer(){
+        searchContainer.append(searchBox)
+        searchContainer.append(submitBtn)
+    }
+    //Configure Fetch params
+    const configObj = {
+        method: 'GET',
+        headers: { 'Content-Type': 'application/json' },
+        mode: 'no-cors'
+    };
 
+    //Pull forecast from API based on city
+    let API = 'https://meta-weather.vercel.app/api/location/search/?query='+location
+    function getlocation(){ 
+    return fetch( API, configObj)
+        .then(response => response.json())
+        .then(data => console.log(data));
+            //get woeid and send to getWeather()
+    }  
 
+    function getWeather(){ 
+    return fetch('https://www.metaweather.com//api/location/'+woeid,
+    {
+        method: 'GET',
+        headers: { 'Content-Type': 'application/json' },
+        mode: 'no-cors'
+    })
+        .then(response => response.json())
+        .then(data => console.log(data));
 
+    }  
+
+});// DOM Content Loaded
 
 
