a832b80b8962c115e55118179692e18c6ec77e07
Jared <jaralleg@amazon.com>
2021-04-24 10:15:35 -0400

    rewrite async functions

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
+//clear previous locations on load
+document.onload = function(){let location = "";
+
 //Grab the containers and assign to a var
 const searchContainer = document.getElementById('search-container');
 const weatherContainer = document.getElementById('weather-container');
+
 //Grab the button , assign to a var and add event listener
 const submitBtn = document.querySelector('#search-container > button');
 submitBtn.addEventListener('click', (e) => { 
-    let location = document.querySelector("#city").value
-    console.log(location)
     e.preventDefault()
+    let location = document.querySelector("#city").value
+    document.getElementById(submitBtn).click();
+    console.log(location)
+    
 });
 
 submitBtn.addEventListener("keyup", (e) => {
@@ -14,6 +20,7 @@ submitBtn.addEventListener("keyup", (e) => {
     if (e.keyCode === 13) {
       e.preventDefault();
       document.getElementById(submitBtn).click();
+      console.log(location)
     }
   });
 
@@ -21,32 +28,29 @@ submitBtn.addEventListener("keyup", (e) => {
 //Pass the city from the form to the fetch and get the woeid for the city
 //Then pass the woeid to the second fetch to get the forecast for the woeid city
 //Configure Fetch params
-const configObj = {
-    method: 'GET',
-    headers: { 'Content-Type': 'application/json' },
-    mode: 'no-cors'
-};
 
 //Pull forecast from API based on city
-let API = 'https://meta-weather.vercel.app/api/location/search/?query='+location
-function getlocation(){ 
- fetch( API, configObj)
-    .then(response => response.json())
-    .then(data => console.log(data[0].woeid))//pull the woeid from there to pass to next fetch
-    
-}  
 
-function getWeather(){ 
-return fetch('https://www.metaweather.com//api/location/'+woeid,
-{
+async function getlocation(){ 
+ fetch('https://meta-weather.vercel.app/api/location/search/?query='+location, {
     method: 'GET',
     headers: { 'Content-Type': 'application/json' },
     mode: 'no-cors'
 })
     .then(response => response.json())
-    .then(data => console.log(data));
-//return the forecastand pass to the functions below
-} 
+    .then(data => console.log(data[0].woeid))//pull the woeid from here to pass to next fetch
+}  
+
+async function getWeather(){
+    fetch('https://meta-weather.vercel.app/api/location/2487889', {
+        method: 'GET',
+        headers: { 'Content-Type': 'application/json' },
+        mode: 'no-cors'
+    })
+        let citydata = await response.json();
+        console.log(citydata);
+    //return the forecast and pass to the functions below
+    }
 
 function createForecast(){
     console.log('forecast placeholder');
@@ -69,3 +73,4 @@ document.addEventListener('DOMContentLoaded', (e) => {
   .then(res => res.json())
   .then(console.log)
 */
+}
