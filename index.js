ac00f9723fca083684bcfc2906f621951a417b06
Jared <jaralleg@amazon.com>
2021-04-25 21:35:06 -0400

    added darkmode

--- index.js
+++ index.js
@@ -1,53 +1,93 @@
-//Grab the containers and assign to a var
+
+function addDarkMode(){ //TODO: Break this into single functions
+const darkModeContainer = document.getElementById('darkMode-container')
+const darkModeCheck = document.createElement("INPUT");
+darkModeCheck.setAttribute("type", "checkbox")
+darkModeCheck.setAttribute("id", "checkbox")
+let toggler = document.createElement('p')
+let togglerText = document.createTextNode("Toggle Darkmode");
+toggler.append(togglerText)
+darkModeContainer.append(darkModeCheck)
+darkModeContainer.append(toggler)
+darkModeCheck.addEventListener('click', () => {  
+darkModeCheck.addEventListener('change', function() {
+        if (this.checked) {
+
+            document.body.style.backgroundColor = "black";
+            document.body.style.color = "white";
+            searchContainer.style.backgroundImage = "none";
+            console.log("Checkbox is checked..");
+        } else {
+            document.body.style.backgroundColor = "white";
+            document.body.style.color = "black";
+            searchContainer.style.backgroundImage = 'url(garden.jpg)';
+        }
+        });
+})
+}
+addDarkMode()
+
 const searchContainer = document.getElementById('search-container');
 const weatherContainer = document.getElementById('weather-container');
 const submitBtn = document.getElementById('submit')
 
-async function getlocation(){ 
- const response = await fetch('https://meta-weather.vercel.app/api/location/search/?query=san+diego', {
+
+submitBtn.addEventListener('click', () => { 
+    getlocation()
+    let cityValue = document.getElementById('city').value
+    console.log('Fetching woeid for ' + cityValue)
+})
+
+    async function getlocation(){                                                            // + cityValue
+        const response = await fetch('https://meta-weather.vercel.app/api/location/search/?query=new+york', {
     })
    const data = await response.json()
    let WID = data[0].woeid
+   console.log('The woeid for that city is: ' + WID)
+   console.log('Fetching the weather for: ' + WID)
 
-    async function getWeather(){
-      let weather = await fetch(`https://meta-weather.vercel.app/api/location/`+ WID, {
+    async function getWeather(){                                               // + WID
+      const weatherRes = await fetch(`https://meta-weather.vercel.app/api/location/2487889`, {
             method: 'GET',
             headers: { 'Content-Type': 'application/json' },
+            Body : 'consolidated_weather',
             mode: 'no-cors'
         })
-            let cityData = await weather.json()
-            console.log(cityData)    
+        const weatherData = await weatherRes.consolidated_weather
+        let forecast = data[0].consolidated_weather
+        console.log(forecast)
         }
         getWeather()
+
+    function appendForecast(){
+        console.log('forecast placeholder');
+        let fiveDayForecast = 'Your 5 Day forcast:'
+        weatherContainer.append('fiveDayForecast')
+    }
+    appendForecast()
 }
 
-    submitBtn.addEventListener('click', () => { 
-        getlocation()
-        //getWeather()
-        let location = document.getElementById('city').value
-        console.log(location)
-    })
 
 
+  
+//TODO: Like and dislike buttons 
+  
+// const likeBtn = document.getElementById('like-button')
+// const dislikeBtn = document.getElementById('dislike-button')
+// const likeCount = document.createElement('p')
+// const dislikeCount = document.createElement('p')
 
-
-
-// function createForecast(){
-//     console.log('forecast placeholder');
-//     //use data from fetch to make a fiveDayForecast
+// function appendLikeStuff(){
+// searchContainer.append(likeCount)
+// searchContainer.append(dislikeCount)
 // }
+// appendLikeStuff()
+// likeBtn.addEventListener("click", function() {
+// let numberOfLikes = numberOfLikes + 1;
+// likeCount.append(numberOfLikes)
+// });
 
-// function appendForecast(){
-//     console.log('forecast placeholder');
-//     weatherContainer.append('fiveDayForecast')
-// }
-/*
-submitBtn.addEventListener("keyup", (e) => {
-    // Number 13 is the "Enter" key on the keyboard
-    if (e.keyCode === 13) {
-      e.preventDefault();
-      document.getElementById(submitBtn).click();
-      console.log(location)
-    }
-  });
-*/
+// dislikeBtn.addEventListener("click", function() {
+// let numberOfHates = numberOfHates + 1;
+// dislikeCount.append(numberOfHates)
+// });
